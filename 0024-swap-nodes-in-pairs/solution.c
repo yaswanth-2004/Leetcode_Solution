5b35a502e8360cfbe663a4eaa7236952b4b56169
Yaswanth Raj E <124978962+yaswanth-2004@users.noreply.github.com>
2024-05-27 15:12:04 +0000

    [LeetCode Sync] Runtime - 3 ms (0.41%), Memory - 5.7 MB (100.00%)

---
+++ 0024-swap-nodes-in-pairs/solution.c
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* swapPairs(struct ListNode* head) {
+    if(head==NULL || head->next == NULL){
+        return head;
+    }
+    struct ListNode* temp=head->next;
+    head->next=swapPairs(head->next->next);
+    temp->next=head;
+
+    return temp;
+}
