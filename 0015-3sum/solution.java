47e778134b51f4166ea28ba09081ec27fa829ac4
Yaswanth Raj E <124978962+yaswanth-2004@users.noreply.github.com>
2024-05-30 18:10:06 +0000

    [LeetCode Sync] Runtime - 840 ms (12.86%), Memory - 52 MB (35.99%)

--- 0015-3sum/solution.java
+++ 0015-3sum/solution.java
@@ -1,43 +1,26 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-
+        int target = 0;
         Arrays.sort(nums);
-
-        for(int i=0;i<nums.length-2;i++)
-        {
-            if(i>0 && nums[i]==nums[i-1]){
-                continue;
-            }
-
-            int j=i+1;
-            int k=nums.length-1;
-
-
-            while(j<k){
-                int sum = nums[i]+nums[j]+nums[k];
-
-                if(sum==0){
-                    
-                    while(j<k && nums[j]==nums[j+1]){
-                        j++;
-                    }
-                    while(j<k && nums[k]==nums[k-1]){
-                        k--;
-                    }
-                    ans.add(Arrays.asList(nums[i],nums[j],nums[k]));
-
+        Set<List<Integer>> s = new HashSet<>();
+        List<List<Integer>> output = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++){
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == target) {
+                    s.add(Arrays.asList(nums[i], nums[j], nums[k]));
                     j++;
                     k--;
-                }
-                else if(sum<0){
+                } else if (sum < target) {
                     j++;
-                }
-                else{
+                } else {
                     k--;
                 }
             }
         }
-        return ans;
+        output.addAll(s);
+        return output;
     }
 }
