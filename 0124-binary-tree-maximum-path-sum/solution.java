3475f06d92ae3dbf1532ad8437ad44859b273182
Yaswanth Raj E <124978962+yaswanth-2004@users.noreply.github.com>
2024-11-27 07:53:46 +0000

    [LeetCode Sync] Runtime - 1 ms (12.11%), Memory - 44.6 MB (22.26%)

---
+++ 0124-binary-tree-maximum-path-sum/solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    private int maxSum = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        maxGain(root);
+        return maxSum;
+    }
+
+    private int maxGain(TreeNode node) {
+        if (node == null) return 0;
+
+        int leftGain = Math.max(maxGain(node.left), 0);
+        int rightGain = Math.max(maxGain(node.right), 0);
+
+        int priceNewPath = node.val + leftGain + rightGain;
+        maxSum = Math.max(maxSum, priceNewPath);
+        return node.val + Math.max(leftGain, rightGain);
+    }
+}
