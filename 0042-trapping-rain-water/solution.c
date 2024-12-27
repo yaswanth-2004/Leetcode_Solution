5f11be9ac3dc0b81706a7b1d360af15b2def156d
Yaswanth Raj E <124978962+yaswanth-2004@users.noreply.github.com>
2024-12-27 13:35:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.7 MB (32.11%)

--- 0042-trapping-rain-water/solution.c
+++ 0042-trapping-rain-water/solution.c
@@ -1,36 +1,15 @@
-int trap(int* a, int n) {
-    int l=0;
-    int r=n-1;
-    int lm=0;
-    int rm=0;
-    int res=0;
-    while(l<r)
-    {
-        if(a[l]<a[r])
-        {
-        if(lm<a[l])
-        {
-            lm=a[l];
-        }
-        else
-        {
-            res+=lm-a[l];
-        }
-        l++;
-        }
-        else
-        {
-            if(a[r]>rm)
-            {
-                rm=a[r];
-            }
-            else
-            {
-                res+=rm-a[r];
-            }
-            r--;
-        }
-    }
-    return res;
+int trap(int* height, int heightSize) {
+    int level=0;
+    int water=0;
 
+    while(heightSize--){
+        int lower=*height < height[heightSize] ? *height++:height[heightSize];
+
+        if(level<lower){
+            level=lower;
+        }
+
+        water+=level-lower;
+    }
+    return water;
 }
