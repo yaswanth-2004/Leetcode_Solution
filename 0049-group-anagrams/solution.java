7f8bbd088f31baf52c5c626d57998792f6c4d97c
Yaswanth Raj E <124978962+yaswanth-2004@users.noreply.github.com>
2024-12-28 14:19:18 +0000

    [LeetCode Sync] Runtime - 20 ms (16.16%), Memory - 48.5 MB (19.47%)

--- 0049-group-anagrams/solution.java
+++ 0049-group-anagrams/solution.java
@@ -1,18 +1,26 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String,List<String>> str = new HashMap<>();
+        Map<String, List<String>> ans = new HashMap<>();
 
-        for(String word:strs){
-            char[] arr = word.toCharArray();
-            Arrays.sort(arr);
-            String sorted=new String(arr);
-              if(!str.containsKey(sorted)){
-            str.put(sorted,new ArrayList<>());
-        }
-        str.get(sorted).add(word);
+        for (String s : strs) {
+            int[] count = new int[26];
+
+            // Count frequency of each letter in the string
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int num : count) {
+                sb.append(num).append("#");
+            }
+            String key = sb.toString();
+            if (!ans.containsKey(key)) {
+                ans.put(key, new ArrayList<>());
+            }
+            ans.get(key).add(s);
         }
 
-      
-        return new ArrayList<>(str.values());
+        return new ArrayList<>(ans.values());        
     }
 }
